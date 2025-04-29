b3c5fbbc2919e1b67680f3aa6ea1815e72141dfa
rjimenez <r.jimenez.2021@alumnos.urjc.ess>
2025-04-29 22:05:38 +0200

    Versión 4: Retorno al inicio (nueva plantilla)

--- P4/informacion.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("node-version").innerText     = process.versions.node;
-    document.getElementById("electron-version").innerText = process.versions.electron;
-    document.getElementById("chrome-version").innerText   = process.versions.chrome;
-  });
